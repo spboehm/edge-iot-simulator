0086aa93925c7b23758daaf723144a3b9c213874
Sebastian Böhm <sebastian.boehm@uni-bamberg.de>
2021-11-23 14:55:58 +0100

    initial implementation of cpu load simulator

---
+++ edge_iot_simulator/core/cpu_load_svc.py
@@ -0,0 +1,138 @@
+from enum import Enum
+import json
+import logging
+import multiprocessing
+from multiprocessing import process
+import os
+import signal
+import threading
+import time
+import psutil
+import queue
+from cpu_load_generator import load_single_core, load_all_cores, from_profile
+from messaging.mqtt_publisher import MqttMessage
+
+logging.basicConfig(format='%(asctime)s %(module)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
+class CPULoadService(threading.Thread):
+
+    def __init__(self, consumer_queue):
+        threading.Thread.__init__(self)
+        self.consumer_queue = consumer_queue
+        self.lock = threading.Lock()
+        self.exit_event = threading.Event()
+        self.logger = logging.getLogger(__name__)
+        self.proc = None
+        self.CPULoadJobQueue = queue.Queue()
+        self.CPULoadJobHistory = {}
+        self.job_count = 0
+        self.CPULoadJobRunner = None
+        self.current_job_id = None
+
+    def run(self):
+        self.logger.debug("Successfully started CPULoadService...")
+        self.CPULoadJobRunner = threading.Thread(target=self.run_cpu_load_job, args=())
+        self.CPULoadJobRunner.start()
+        while not self.exit_event.is_set():
+            item = self.consumer_queue.get()
+
+            new_cpu_load_job_all_cores = None
+            if (isinstance(item, CPULoadJobAllCores)):
+                new_cpu_load_job_all_cores = item
+            elif (item == 'shutdown'):
+                return
+            else:
+                self.logger.error('Unknown message received!')
+
+            self.logger.info('Received new CPULoadJobAllCores: {}'.format(new_cpu_load_job_all_cores.to_string()))
+            with self.lock:
+                self.job_count += 1
+                created_cpu_load_job_all_cores = QueuedCPULoadJobAllCores(new_cpu_load_job_all_cores.duration, new_cpu_load_job_all_cores.target_load, self.job_count, None, CPULoadJobStates.CREATED.name)
+                self.CPULoadJobHistory[created_cpu_load_job_all_cores.id] = created_cpu_load_job_all_cores
+            self.CPULoadJobQueue.put(created_cpu_load_job_all_cores)
+
+    def run_cpu_load_job(self):
+        self.logger.debug('Successfully started CPULoadJobRunner...')
+        while not self.exit_event.is_set():
+            self.logger.debug('CPULoadJobRunner is waiting for incoming jobs...')
+            item = self.CPULoadJobQueue.get()
+
+            new_queued_cpu_load_job_all_cores = None
+            if (isinstance(item, QueuedCPULoadJobAllCores)):
+                new_queued_cpu_load_job_all_cores = item
+                self.logger.info('Received new QueuedCPULoadJobAllCores: {}'.format(new_queued_cpu_load_job_all_cores.to_string()))            
+            elif (item == 'shutdown'):
+                return
+            else:
+                self.logger.error('Unknown message received!')
+
+            self.proc = multiprocessing.Process(target=load_all_cores, args=(new_queued_cpu_load_job_all_cores.duration, new_queued_cpu_load_job_all_cores.target_load))
+            self.proc.start()
+            current_pid = self.proc.pid
+            with self.lock:
+                queued_cpu_load_job_all_cores = QueuedCPULoadJobAllCores(new_queued_cpu_load_job_all_cores.duration, new_queued_cpu_load_job_all_cores.target_load, new_queued_cpu_load_job_all_cores.id, current_pid, CPULoadJobStates.RUNNING.name)
+                self.CPULoadJobHistory[queued_cpu_load_job_all_cores.id] = queued_cpu_load_job_all_cores
+                self.current_job_id = queued_cpu_load_job_all_cores.id
+            self.logger.info('Started new QueuedCPULoadJobAllCores {}'.format(queued_cpu_load_job_all_cores.to_string()))
+            self.proc.join()
+            with self.lock:
+                if (self.CPULoadJobHistory[self.current_job_id].state != CPULoadJobStates.ABORTED.name):
+                    self.CPULoadJobHistory[self.current_job_id].state = CPULoadJobStates.TERMINATED.name
+                    self.logger.info('{} QueuedCPULoadJobAllCores {}'.format(self.CPULoadJobHistory[self.current_job_id].state, queued_cpu_load_job_all_cores.to_string()))
+
+    def stop_current_CPULoadJob(self):
+        if self.proc is not None and self.proc.pid > 0:
+            self.logger.info('Stop current CPULoadJob with parent pid {}'.format(self.proc.pid))
+            with self.lock:
+                self.CPULoadJobHistory[self.current_job_id].state = CPULoadJobStates.ABORTED.name
+            parent = psutil.Process(self.proc.pid)
+            current_process = os.getpid()
+            children = parent.children(recursive=True)
+            for p in children:
+                if (p.pid != current_process):
+                    p.terminate()
+            self.proc.terminate()
+            self.proc.join()
+            self.logger.info('Successfully stopped CPULoadJob with parent pid {}'.format(self.proc.pid))
+            self.proc = None
+
+    def stop(self):
+        self.logger.info('CPU load service received shutdown signal...')
+        if self.proc is not None and self.proc.pid > 0 and self.proc.is_alive():
+            self.stop_current_CPULoadJob()
+        self.exit_event.set()
+        self.consumer_queue.put('shutdown')
+        self.CPULoadJobQueue.put('shutdown')
+        self.CPULoadJobRunner.join()
+        if (self.lock.locked()):
+            self.lock.release()
+
+
+class CPULoadJobAllCores():
+
+    def __init__(self, duration, target_load):
+        self.duration = duration
+        self.target_load = target_load
+
+    def to_json(self):
+        return json.dumps(self.__dict__)
+
+    def to_string(self):
+        return str(self.to_json())
+
+class QueuedCPULoadJobAllCores(CPULoadJobAllCores):
+
+    def __init__(self, duration, target_load, id, pid, state):
+        super().__init__(duration, target_load)
+        self.id = id
+        self.pid = pid
+        self.state = state
+
+class CPULoadJobStates(Enum):
+    CREATED = 1
+    RUNNING = 2
+    ABORTED = 3
+    TERMINATED = 4
+
+class CPULoadServiceServiceException(Exception):
+    pass
--- edge_iot_simulator/main.py
+++ edge_iot_simulator/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from core.temperature_svc import TemperatureMeasurement, TemperatureService,TemperatureUnits
+from core.cpu_load_svc import CPULoadJobAllCores, CPULoadService
 from messaging.mqtt_publisher import MqttException, MqttPublisher, MqttStatus
 from web.app import WebApp
 
@@ -18,20 +19,30 @@ load_dotenv()
 
 if __name__=="__main__":
 
-    queue = queue.Queue()
+    publisher_queue = queue.Queue()
+    consumer_queue = queue.Queue()
 
-    publisher = MqttPublisher(queue)
-    temperature_svc = TemperatureService(queue, 3, TemperatureUnits.celsius.name)
+    publisher = MqttPublisher(publisher_queue)
+    temperature_svc = TemperatureService(publisher_queue, 3, TemperatureUnits.celsius.name)
+    cpuload_svc = CPULoadService(consumer_queue)
     web_app = WebApp(publisher, temperature_svc)
     
     try:
-        logging.info('Please any key to interrupt...')
+        logging.info('Please press any key to interrupt...')
         publisher.start()
         temperature_svc.start()
+        cpuload_svc.start()
         web_app.start()
         time.sleep(5) # wait for connection to mqtt broker
         if(publisher.get_mqtt_statistics().status == MqttStatus.disconnected.name):
-            raise Exception("Could not establish the connection to the mqtt broker...")
+            raise Exception("Could not establish the connection to the mqtt broker...") 
+
+        time.sleep(5)
+        consumer_queue.put(CPULoadJobAllCores(15, 0.65))
+        time.sleep(5)
+        cpuload_svc.stop_current_CPULoadJob()
+        consumer_queue.put(CPULoadJobAllCores(20, 0.80))
+
         signal.pause()
     except KeyboardInterrupt as e:
         logging.info("Received user's shutdown signal...")
@@ -40,12 +51,14 @@ if __name__=="__main__":
     finally:
         publisher.stop()
         temperature_svc.stop()
+        cpuload_svc.stop()
         web_app.stop()
 
         logging.info('Wait for graceful termination...')
         
         publisher.join()
         temperature_svc.join()
+        cpuload_svc.join()
         web_app.join()
 
     logging.info('Successfully terminated edge-iot simulator')
